704ef5f052c2ef38b5484b1c0f75823e5fcabf45
hellothere402 <archiejohnbruce@outlook.com>
2024-06-17 12:37:02 +1200

    n

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import { Speech, Physical, Cognitive, Auditory} from './containers'
-
+import { Speech, Physical, Cognitive, Auditory, About, Contact} from './containers'
+import Navbar from './components/navbar/navbar'
 const App = () => (
-    <div className="App"></div>
+    <div className="App">
+<Navbar />
+<div/>
+<Speech />
+ <Physical />
+ <Cognitive />
+ <Auditory />
+ <About />
+ <Contact />
+ </div>
+
 )
